4f065888928ef0d7c391bee512e38763d0c1762c
NathanDoile <doile.souza@gmail.com>
2026-07-30 16:48:12 -0300

    Alterando versao JDK para uma com suporte JaCoCo

--- src/main/java/br/com/ubots/flowpay/FlowpayApplication.java
+++ src/main/java/br/com/ubots/flowpay/FlowpayApplication.java
@@ -8,7 +8,7 @@ import org.springframework.resilience.annotation.EnableResilientMethods;
 @EnableResilientMethods
 public class FlowpayApplication {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
 		SpringApplication.run(FlowpayApplication.class, args);
 	}
 
