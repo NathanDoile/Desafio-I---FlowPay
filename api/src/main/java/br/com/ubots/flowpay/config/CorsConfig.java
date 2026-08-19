8761884689c9673fcd36b4e4481a82e797fda19d
NathanDoile <doile.souza@gmail.com>
2026-08-19 19:51:21 -0300

    Arrumando cors

--- api/src/main/java/br/com/ubots/flowpay/config/CorsConfig.java
+++ api/src/main/java/br/com/ubots/flowpay/config/CorsConfig.java
@@ -9,12 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
 
     @Value("${app.cors.allowed-origins}")
-    private String[] allowedOrigins;
+    private String allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns(allowedOrigins)
+                .allowedOrigins(allowedOrigins.split(","))
                 .allowedMethods("GET","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
